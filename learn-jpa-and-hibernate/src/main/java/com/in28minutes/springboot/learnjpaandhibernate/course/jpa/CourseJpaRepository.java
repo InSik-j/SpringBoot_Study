8bb42adf8fdcb0d257b1d441b74a32dc9ad6a819
InSik-j <junginsik8642@gmail.com>
2024-03-21 19:48:31 +0900

    섹션6_JPA와 EntityManager 그리고 Spring Data JPA 알아보기

---
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jpa/CourseJpaRepository.java
@@ -0,0 +1,31 @@
+package com.in28minutes.springboot.learnjpaandhibernate.course.jpa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+@Repository
+@Transactional // JPA로 쿼리를 실행할 때마다 트랜잭션을 허용
+public class CourseJpaRepository {
+	
+	@PersistenceContext // @Autowired보다 더 구체적인 어노테이션 
+	private EntityManager entityManager;
+	
+	public void insert(Course course) {
+		entityManager.merge(course);
+	}
+	
+	public Course findById(long id) {
+		return entityManager.find(Course.class, id);
+	}
+	
+	public void deleteById(long id) {
+		Course course = entityManager.find(Course.class, id);
+		entityManager.remove(course);;
+	}
+}
